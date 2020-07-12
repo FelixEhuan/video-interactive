b3d53aad407b4a37552d8d3a5f7af4d2d295ba75
Felix Ehuan <fnixro2@gmail.com>
2020-07-11 21:19:01 -0500

    feat: add docs

---
+++ docs/precache-manifest.775edadaecc6f8fccf21a08270d1a46c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb794e5d071a8feff88fea586ef23ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a84ea7c0fc212d6caa87",
+    "url": "/static/css/main.b064430b.chunk.css"
+  },
+  {
+    "revision": "14926e1e56585d284c4a",
+    "url": "/static/js/2.58626e16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58626e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84ea7c0fc212d6caa87",
+    "url": "/static/js/main.581840b4.chunk.js"
+  },
+  {
+    "revision": "7ef7dfdaf58c42cb3837",
+    "url": "/static/js/runtime-main.9f843ba1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "07c5ee4b5c66fbdce9a217f5381ab24b",
+    "url": "/static/media/seyba.07c5ee4b.png"
+  }
+]);
