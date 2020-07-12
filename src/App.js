7bb8a86eb4abf0127ac4d8877e3429378a66d62f
Felix Ehuan <fnixro2@gmail.com>
2020-07-11 21:15:08 -0500

    feat: add image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import seyba from './seyba.png';
 function App() {
+  const clickedBoat = () => {
+    console.log('clickedBoat');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <img src={seyba} className="App-logo" alt="logo" />
+      <button onClick={clickedBoat} className="invisible-click" id="boat"></button>
+    </div> 
   );
 }
 
